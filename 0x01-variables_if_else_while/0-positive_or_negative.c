fc70f805131486b6a4a680ed8f37b89b43c9f18e
Okoroafor Daniel <okoroafordaniel0@gmail.com>
2022-09-09 10:48:33 -0700

    my commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+* main - print if the number is positive, zero, or negative
+*
+* Description: using the main function
+* this program prints programming is positive, zero, or negative
+* Return: 0
+*/
 int main(void)
 {
 int n;
