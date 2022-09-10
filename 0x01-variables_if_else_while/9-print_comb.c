86649615f7772fdfa4af468880595e9528d6570d
Okoroafor Daniel <okoroafordaniel0@gmail.com>
2022-09-09 17:54:36 -0700

    my commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,20 +5,23 @@
  */
 int main(void)
 {
-int num;
+
+	int num;
 
 for (num = '0'; num <= '9'; num++)
 {
 	putchar(num);
-if (num == '9')
-{
-break;
-}
-else
-{
-	putchar(',');																				putchar(' ');
-}
+	if (num == '9')
+	{
+		break;
+	}
+	else
+	{
+		putchar(',');
+
+		putchar(' ');
+	}
 }
 	putchar('\n');
-return (0);
+	return (0);
 }
