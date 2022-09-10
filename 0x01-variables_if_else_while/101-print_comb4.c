788418302db518bdb43320f7f70e8da8fc5bf8d7
Okoroafor Daniel <okoroafordaniel0@gmail.com>
2022-09-09 19:03:17 -0700

    my commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,9 +6,9 @@
 */
 int main(void)
 {
-int i = '0';
-int j = '0';
-int k = '0';
+	int i = '0';
+	int j = '0';
+	int k = '0';
 while (i <= '7')
 {
 while (j <= '8')
@@ -17,23 +17,23 @@ while (k <= '9')
 {
 if (i < j && j < k)
 {
-putchar(i);
-putchar(j);
-putchar(k);
+	putchar(i);
+	putchar(j);
+	putchar(k);
 if (!(i == '7' && j == '8' && k == '9'))
 {
-putchar(',');	
-putchar(' ');
+	putchar(',');	
+	putchar(' ');
 }
 }
-k++;
+	k++;
 }
-k = '0';
-j++;
+	k = '0';
+	j++;
 }
-j = '0';
-i++;
+	j = '0';
+	i++;
 }
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
