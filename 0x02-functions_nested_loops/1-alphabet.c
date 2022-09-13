511056e0a5a1ac825ed3dfbe34960ef4f756cff7
Okoroafor Daniel <okoroafordaniel0@gmail.com>
2022-09-13 11:05:19 -0700

    my commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	char n = 'a';
+
+	while(n <= 'z')
+	{
+		_putchar(n);
+			n++;
+
+	}
+	_putchar("\n");
+}
