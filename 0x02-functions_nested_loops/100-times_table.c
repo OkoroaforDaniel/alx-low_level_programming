db83528a56eabc06694bee2801c8e4da9431b1ba
Okoroafor Daniel <okoroafordaniel0@gmail.com>
2022-09-13 15:14:29 -0700

    my commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,8 +19,10 @@ void print_times_table(int n)
 			_putchar(48);
 			for (b = 1; b <= n; b++)
 			{
-				op = a * b;																				_putchar(44);
-				_putchar(32);																				if (op <= 9)
+				op = a * b;
+				_putchar(44);
+				_putchar(32);
+				if (op <= 9)
 				{
 					_putchar(32);																				_putchar(32);
 					_putchar(op + 48);
